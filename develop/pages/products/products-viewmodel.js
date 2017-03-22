a04800ce23225ac49bb5f6e44262fc5004fb3153
PGS-SOFT\mgierczak <mgierczak@pgs-soft.com>
2017-03-22 14:33:01 +0100

    aa

--- develop/pages/products/products-viewmodel.js
+++ develop/pages/products/products-viewmodel.js
@@ -3,12 +3,23 @@ var ProductsViewModel = function (filter) {
     self.products = [];
     self.productsList = ko.observableArray(self.products);
     self.productsJson = 'https://frammapp-knockout.firebaseio.com/.json'; //'https://project-5613440220430148247.firebaseio.com/api/v1/categories.json';
+
+    self.filter = ko.observable('promoted');
     var dm = new DataModel($.getJSON, $.map);
     dm.getProducts(self.productsJson, self.productsList);
+    dm.getProductsFiltered(self.filter(), true);
 
     self.chosenProduct = ko.observable();
     self.getProduct = function(elem) {
         self.chosenProduct(elem.guid);
+    };
+
+ 
+    
+    self.resetFilter = function() {
+        self.filter = ko.observable('');
+        dm.getProducts(self.productsJson, self.productsList);
+        dm.getProductsFiltered(self.filter(), true);
     };
 
 };
